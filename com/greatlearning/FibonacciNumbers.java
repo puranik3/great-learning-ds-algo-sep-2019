b5d333fb9612ef52ebec3ac5ca1b103408490b20
Prashanth Puranik <puranik@digdeeper.in>
2019-09-14 14:15:00 +0530

    started fibonacci

---
+++ com/greatlearning/FibonacciNumbers.java
@@ -0,0 +1,10 @@
+package com.greatlearning;
+
+public class FibonacciNumbers {
+    public static void main(String[] args) {
+        BigInt x = new BigInt( "0" );
+        BigInt y = new BigInt( "1" );
+
+        for( int i = 2; i <= 100; i++ ) {}
+    }
+}
