64750a0725cd661a4f6cf9c103d0faa1d7cb0123
Prashanth Puranik <puranik@digdeeper.in>
2019-09-15 10:00:22 +0530

    queue implementation pending

--- com/greatlearning/LinkedListQueue.java
+++ com/greatlearning/LinkedListQueue.java
@@ -3,8 +3,19 @@ package com.greatlearning;
 public class LinkedListQueue implements Queue {
     LinkedList queue = new LinkedList();
 
-    public void enqueue( Object data );
-    public Object dequeue();
-    public Object peek();
-    public int size();
+    public void enqueue( Object data ) {
+
+    }
+
+    public Object dequeue() {
+
+    }
+
+    public Object peek() {
+
+    }
+
+    public int size() {
+        
+    }
 }
